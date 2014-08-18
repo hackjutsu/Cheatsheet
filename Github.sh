84930e87530632637cf9614fcee409a2a2af6ff4
registercosmo <cosmoqiu@Cosmos-MacBook-Pro.local>
2014-08-18 16:44:33 -0700

    Add some basic commands about Github.
    DONE:
    - init, pull, remote add origin, add, status, commit, push
    
    TODO:
    - Not sure when to use “git init”, should we use it every time we want to submit sth ?
    - Not sure when to use “git remote add origin”. Same question as above.
    - Could we use some name other than “origin” ?
    - What is the meaning of “-u” in the “git push”command ?
    - Need the commands to change the account.

---
+++ Github.sh
@@ -0,0 +1,7 @@
+git init 
+git pull https://github.com/registercosmo/Cheatsheet.git
+git remote add origin https://github.com/registercosmo/Cheatsheet.git
+git add .
+git status
+git commit
+git push -u origin master
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Cheatsheet
 ==========
+
+8/18/2014
+    - add cheatsheet about Github commands
