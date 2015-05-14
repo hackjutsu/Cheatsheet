956d9b8fc5014fa953815836d1424c849f479c35
registercosmo <registercosmo@gmail.com>
2015-05-14 15:03:56 -0700

    more Git commands

--- Github.sh
+++ Github.sh
@@ -9,3 +9,20 @@ git push -u origin master
 
 // To pull changes
 git pull origin master
+
+
+// ABOUT CONFIGS
+// Working with Config globally/locally
+git config --global user.name="My username"
+git config --gloabl user.email=myemail@example.com
+git config --local user.name="My username"
+git config --local user.email=myemail@example.com
+
+// List all the configs
+git config --list
+// Remove the configs
+git config --global --unset-all user.name
+// Change the configs
+git config --global --replace-all user.name "New User Name"
+
+
